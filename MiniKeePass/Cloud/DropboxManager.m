a1a59c0e6582ef7460d9f9187a3095931e43ee90
Tait Smith <tssmith@Taits-MacBook-Pro.local>
2017-05-22 13:05:10 -0700

    Small tweaks to make compiler warnings disappear.

--- MiniKeePass/Cloud/DropboxManager.m
+++ MiniKeePass/Cloud/DropboxManager.m
@@ -15,7 +15,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#import <ObjectiveDropboxOfficial/ObjectiveDropboxOfficial.h>
+#import "ObjectiveDropboxOfficial/ObjectiveDropboxOfficial.h"
 #import "DropboxManager.h"
 #import "KeychainUtils.h"
 
